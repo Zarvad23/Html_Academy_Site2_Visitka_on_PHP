8e270fc2a3801623dec923417decf04f616065b4
Vadim Zarifullin <65528175+Zarvad23@users.noreply.github.com>
2021-08-11 16:42:20 +0500

    Добавлен сайт

---
+++ components/about.php
@@ -0,0 +1,8 @@
+<section class="about container">
+  <h2 class="section-title">Привет, я <?= $name ?></h2>
+  <img class="about-image" src="<?= $image ?>" width="147" height="147" alt="<?= $name ?>">
+  <div class="about-content">
+    <p>Я начинающий веб разработчик, на данный момент учусь в колледже на 3-ем курсе по специальности информационные системы и программирование</p>
+    <p>В будущем надеюсь стать умелым разработчиком сайтов , игр или еще чего либо</p>
+  </div>
+</section>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Сайт Вадима';
+$name = 'Зарифуллин Вадим';
+$image = 'img/4e4425929a600664b33518d5f82cb550.jpg';
+$email = 'zarifullin.vadim@mail.ru';
+$phone = '+79825510453';
+
+require('components/header.php');
+require('components/status.php');
+require('components/about.php');
+require('components/photos.php');
+require('components/news.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer.php');
